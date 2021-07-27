a8124a0432e3bf98ab84bc233fa2d38ac256acef
Sazad Ahemad <mail2sazad@gmail.com>
2021-07-27 23:33:13 +0530

    updated

---
+++ page.php
@@ -0,0 +1,7 @@
+<?php
+    include_once("components/header.php");
+    include_once("components/featured.php");
+    include_once("components/body.php");
+    include_once("components/header.php");
+    include_once("components/footer.php");
+?>
